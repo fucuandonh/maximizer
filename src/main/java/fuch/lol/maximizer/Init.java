6310681625c187e5cf2605b5f1fa18dacd20a662
fcuandonh <fcuandonh@gmail.com>
2024-06-01 05:04:58 -0600

    fix: delete unnecessary for

--- src/main/java/fuch/lol/maximizer/Init.java
+++ src/main/java/fuch/lol/maximizer/Init.java
@@ -1,10 +1,8 @@
 package fuch.lol.maximizer;
 
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fuch.lol.maximizer.data.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -12,8 +10,5 @@ import lombok.extern.slf4j.Slf4j;
 public class Init {
 	public static void main(String[] args) {
 		SpringApplication.run(Init.class, args);
-		for (JSONObject championData : Data.getChampionsData()) {
-			log.info("Champion: {}", championData.get("name"));	
-		}
 	}
 }
